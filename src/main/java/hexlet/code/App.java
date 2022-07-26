de26bea7d848544f2702e7959e791825457a1e17
IlnazKamalov <kamalovpk@gmail.com>
2022-07-26 12:08:19 +0300

    Choice the Game

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,10 +1,21 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 import static hexlet.code.Cli.getName;
 
 public class App {
-    public static void  main(String[] args) {
-        System.out.println("Welcome to the Brain Game!");
-        getName();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                Please enter the game number and press Enter
+                1 - Greet
+                0 - Exit""");
+        System.out.print("Your choice: ");
+        int choiceNumber = scanner.nextInt();
+        if (choiceNumber == 1) {
+            System.out.println("Welcome to the Brain Game!");
+            getName();
+        }
     }
 }
