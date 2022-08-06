d4ba64c78f095e37d9d5dcd52774513ddf8f774c
IlnazKamalov <kamalovpk@gmail.com>
2022-08-06 09:29:40 +0300

    fix pime

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -21,7 +21,7 @@ public class Prime {
 
     private static boolean isPrime(int primeNumber) {
 
-        if (primeNumber ==1 || primeNumber == 0) {
+        if (primeNumber == 1 || primeNumber == 0) {
             return false;
         }
 
