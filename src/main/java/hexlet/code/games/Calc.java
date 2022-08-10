7313d4efa1cf666268f5cd2d0967c4b61ea0ec9a
IlnazKamalov <kamalovpk@gmail.com>
2022-08-10 08:15:39 +0300

    fix calc

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -41,9 +41,10 @@ public class Calc {
                     result = numberFirst * numberSecond;
                     return result;
                 }
+                default -> System.out.println("No logic defined for operator! :-(");
             }
         } catch (RuntimeException e) {
-            System.out.println(e + "No logic defined for operator! :-(");
+            System.out.println(e.getMessage());
         }
         return result;
     }
