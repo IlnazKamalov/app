14a407756051c1b844ee6d7ba12e0e8b8898ac9b
IlnazKamalov <kamalovpk@gmail.com>
2022-08-16 18:59:54 +0300

    fix prime

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -26,7 +26,7 @@ public class Prime {
         }
 
         for (int i = 2; i < primeNumber / 2; ++i) {
-            if (primeNumber % i == 0) {
+            if ((primeNumber % i) == 0) {
                 return false;
             }
         }
